9ff020254246b884e883b4a0bdb36ce6ed82748f
Doğan Çeçen <sepeth@hey.com>
2026-06-01 17:03:00 +0200

    Be able to run nd alias, nix develop, from any subdirectory

--- fish/functions/nd.fish
+++ fish/functions/nd.fish
@@ -1,3 +1,16 @@
 function nd
-      nix develop path:. -c fish $argv
+    set -l search_dir $PWD
+
+    while true
+        if test -e "$search_dir/flake.nix"
+            nix develop "path:$search_dir" -c fish $argv
+            return $status
+        end
+
+        set -l parent_dir (path dirname "$search_dir")
+        test "$parent_dir" = "$search_dir"; and break
+        set search_dir "$parent_dir"
+    end
+
+    nix develop path:. -c fish $argv
 end
